d28ed54898649e612c4c9d9992b9b58153c514fa
Blake Dempsey <bdempsey864@gmail.com>
2022-04-23 15:56:02 -0400

    working on controllers

---
+++ app/controllers/api/v1/forcast_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::ForcastController < ApplicationController
+  def index
+    require "pry"; binding.pry
+  end
+end
---
+++ spec/requests/api/v1/weather_data/city_weather_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "weather data for a city" do
+  describe "happy paths" do
+    it 'returns weather data for a city' do
+      
+    end
+  end
+end
