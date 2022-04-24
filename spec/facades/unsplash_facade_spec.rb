9b235750af0f743f6cdee26a66722011b264d127
Blake Dempsey <bdempsey864@gmail.com>
2022-04-24 18:36:28 -0400

    Feat: Unsplash facade and facade spec added

---
+++ spec/facades/unsplash_facade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe UnsplashFacade do
+  describe 'picture' do
+    it 'has image data' do
+      location = 'greenville, sc'
+      response = UnsplashFacade.picture(location)
+
+      expect(response).to be_an(Image)
+    end
+  end
+end
