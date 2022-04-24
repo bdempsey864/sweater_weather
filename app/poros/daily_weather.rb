424e511d7831ea7f056084e70ede4fce425c029d
Blake Dempsey <bdempsey864@gmail.com>
2022-04-24 14:17:38 -0400

    Feat: Add Serializer Method

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -8,12 +8,24 @@ class DailyWeather
               :icon
 
   def initialize(data)
-    @date = Time.at(data[:dt]).to_date
+    @date = Time.at(data[:dt])
     @sunrise = Time.at(data[:sunrise])
     @sunset = Time.at(data[:sunset])
     @max_temp = data[:temp][:max]
     @min_temp = data[:temp][:min]
     @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
+  end
+
+  def serialize
+    {
+      date: @date.strftime('%F'),
+      sunrise: @sunrise.to_s,
+      sunset: @sunset.to_s,
+      max_temp: @max_temp,
+      min_temp: @min_temp,
+      conditions: @conditions,
+      icon: @icon
+    }
   end
 end
