e8bc92690af6bae24362aba2fe11401505811aa0
Blake Dempsey <bdempsey864@gmail.com>
2022-04-25 12:52:42 -0400

    services working with hard coded token

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,11 +1,12 @@
 class YelpService 
   def self.conn
-    Faraday.new(url: "https://api.yelp.com")
+    Faraday.new(url: "https://api.yelp.com") do |conn|
+      conn.request :authorization, 'Bearer', 'bduUfztld9gmSGgxxtaZYcuSInci9_GM9ChJya5dFIY2C33sTE1yUtsyhGUgbxKxugicam7-0DCkhwZ0VJv9lo3jocIAM9ccVxG_B_YASVY-pPJE0GlqNd1JfI9lYnYx '
+    end
   end
 
   def self.food_search(food, location)
     response = conn.get("/v3/businesses/search?term=#{food}&location=#{location}")
     JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
   end
 end
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe YelpService do
       response = YelpService.food_search(food, location)
 
       expect(response).to be_a(Hash)
+      expect(response[:businesses]).to be_a(Array)
+      expect(response[:businesses][0]).to be_a(Hash)
+      expect(response[:businesses][0]).to have_key(:name)
+      expect(response[:businesses][0]).to have_key(:coordinates)
+      expect(response[:businesses][0][:coordinates]).to have_key(:latitude)
+      expect(response[:businesses][0][:coordinates]).to have_key(:longitude)
+      expect(response[:businesses][0]).to have_key(:distance)
+      expect(response[:businesses][0][:distance]).to be_a(Float)
     end
   end
 end
